052f11b9c7add2097f4696c98602d6cbf3ce61ae
Kapelo256 <105801565+Kapelo256@users.noreply.github.com>
2024-07-27 12:06:06 -0400

    Your commit message

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,7 @@
+const Pricing = () => {
+    return (
+      <div className="w-full h-screen grid content-center justify-center text-white text-8xl">Pricing</div>
+    )
+  }
+  
+  export default Pricing
